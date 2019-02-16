4891c2baa786a60acc80e554171d2dc1867850e6
Jose Zalacain <Jose Zalacain@AVALON>
2019-02-16 14:33:47 -0400

    Introduce logger implementation

---
+++ src/main/java/org/prolobjectlink/prolog/jpl7/JplLogger.java
@@ -0,0 +1,80 @@
+/*-
+ * #%L
+ * prolobjectlink-jpi-jpl7
+ * %%
+ * Copyright (C) 2012 - 2019 Prolobjectlink Project
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package org.prolobjectlink.prolog.jpl7;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.prolobjectlink.logging.LoggerFormatter;
+import org.prolobjectlink.prolog.AbstractLogger;
+import org.prolobjectlink.prolog.PrologLogger;
+
+public final class JplLogger extends AbstractLogger implements PrologLogger {
+
+	private static final String MESSAGE = "Logger File Handler can't be created";
+	private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+	public JplLogger() {
+		this(Level.INFO);
+	}
+
+	public JplLogger(Level level) {
+		LOGGER.setLevel(level);
+		Logger rootlogger = LOGGER.getParent();
+		SimpleDateFormat f = new SimpleDateFormat("yyyy.MM.dd");
+		String date = f.format(new Date());
+		Formatter formatter = new LoggerFormatter();
+		for (Handler h : rootlogger.getHandlers()) {
+			h.setFormatter(formatter);
+			h.setLevel(level);
+		}
+		FileHandler file = null;
+		try {
+			file = new FileHandler("%t/prolobjectlink-" + date + ".log", true);
+		} catch (SecurityException e) {
+			rootlogger.log(Level.SEVERE, MESSAGE, e);
+		} catch (IOException e) {
+			rootlogger.log(Level.SEVERE, MESSAGE, e);
+		}
+		assert file != null;
+		file.setFormatter(formatter);
+		LOGGER.addHandler(file);
+	}
+
+	public void log(Object sender, Level level, Object message, Throwable throwable) {
+		LOGGER.log(level, sender + "\n" + message, throwable);
+	}
+
+}
